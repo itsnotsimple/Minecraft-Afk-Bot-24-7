f2664b5180433aa71feb170431d82ce902e50dd6
itsnotsimple <99130321+itsnotsimple@users.noreply.github.com>
2022-09-05 01:10:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ function task(i) {
 	
   setTimeout(function() { 
     if(first == true){
-	bot.chat("Subscribe To Easy Gamer : https://www.youtube.com/channel/UCk575VDlrCsv1Gy2XZQ1bSA")
+	bot.chat("SAMO ETZ I TESNI")
 	first = false;
 	}
 	else{
-		bot.chat("Subscribe To MannuG : https://www.youtube.com/channel/UCSZHRnBue50ImBKeR6k6eDg")
+		bot.chat("AIDE KURVI")
 		first = true;
 	}
   }, 3600000 * i); 
